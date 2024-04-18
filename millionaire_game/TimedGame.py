e335426d6933790a4b8b5e3cb85da9f5c469190e
Kamil <sumarakamil89@gmail.com>
2024-04-18 18:04:05 +0200

    My changes

--- millionaire_game/TimedGame.py
+++
@@ -1,3 +0,0 @@
-from game import Game
-
-class
--- millionaire_game/timed_game.py
+++ millionaire_game/timed_game.py
@@ -1,6 +1,24 @@
 # TODO
+from game import Game
 
-class TimedGame:
-    def __init__(self, questions):
+
+class TimedGame(Game):
+    def __init__(self, questions, time_limit):
         super().__init__(questions)
-       
+        self.time_limit = time_limit
+        self.remaining_time = time_limit
+
+    def submit_answer(self, answer):
+        if self.remaining_time <= 0:
+            print("Time's up!")
+            return False
+
+        current_question = self.questions[self._current_question_index - 1]
+        if current_question.check_answer(answer):
+            self._score += 100
+            print(f"Remaining time: {self.remaining_time}")
+            return True
+        else:
+            self.remaining_time -= 10  # Przykładowa kara czasowa
+            print(f"Remaining time: {self.remaining_time}")
+            return False
